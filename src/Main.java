e66e43c87074fcd5ed87bacf9767b98a36ba07f9
zeroCool9011 <andrewmackar0ff@yandex.ru>
2024-12-22 20:30:20 +0300

    Второй коммит

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,28 @@
-import java.util.List;
-import java.util.Scanner;
+import manager.TaskManager;
+import model.TaskStatus;
+import model.Epic;
+import model.SubTask;
+import model.Task;
+
 
 public class Main {
     public static void main(String[] args) {
 
     TaskManager manager = new TaskManager();
 
-    Task task1 = new Task("Task #1","Task1 description", TaskStatus.NEW);
-    Task task2 = new Task("Task #2","Task2 description", TaskStatus.IN_PROGRESS);
+    Task task1 = new Task("model.Task #1","Task1 description", TaskStatus.NEW);
+    Task task2 = new Task("model.Task #2","Task2 description", TaskStatus.IN_PROGRESS);
     final int id1 = manager.addNewTask(task1).getId();
     final int id2 = manager.addNewTask(task2).getId();
 
-    Epic epic1 =  new Epic("Epic#1","Epic1 description",TaskStatus.NEW);
-    Epic epic2 =  new Epic("Epic#2","Epic2 description",TaskStatus.NEW);
+    Epic epic1 =  new Epic("model.Epic#1","Epic1 description");
+    Epic epic2 =  new Epic("model.Epic#2","Epic2 description");
     final int epicId1 = manager.addNewEpic(epic1).getId();
     final int epicId2 = manager.addNewEpic(epic2).getId();
 
-    SubTask subTask1 = new SubTask("SubTask#1","SubTask1 description",TaskStatus.IN_PROGRESS,epicId1);
-    SubTask subTask2 = new SubTask("SubTask#2","SubTask2 description",TaskStatus.NEW, epicId1);
-    SubTask subTask3 = new SubTask("SubTask#3","SubTask3 description",TaskStatus.DONE, epicId2);
+    SubTask subTask1 = new SubTask("model.SubTask#1","SubTask1 description", TaskStatus.IN_PROGRESS,epicId1);
+    SubTask subTask2 = new SubTask("model.SubTask#2","SubTask2 description", TaskStatus.NEW, epicId1);
+    SubTask subTask3 = new SubTask("model.SubTask#3","SubTask3 description", TaskStatus.DONE, epicId2);
         final int subTaskId1 = manager.addNewSubTask(subTask1).getId();
         final int subTaskId2 = manager.addNewSubTask(subTask2).getId();
         final int subTaskId3 = manager.addNewSubTask(subTask3).getId();
@@ -33,9 +37,9 @@ public class Main {
         System.out.println("");
         System.out.println(manager.getEpics());
         System.out.println("");
-
+        manager.deleteSubTasks();
         SubTask subTask = manager.getSubTask(subTaskId3);
-       // subTask.setStatus(TaskStatus.IN_PROGRESS);
+       // subTask.setStatus(model.TaskStatus.IN_PROGRESS);
         //manager.updateSubTask(subTask);
         System.out.println(  manager.getEpicSubTask(3) + " " );
 
@@ -48,6 +52,7 @@ public class Main {
 
 
     final Task task = manager.getTask(2);
+
     task.setStatus(TaskStatus.DONE);
     manager.updateTask(task);
     System.out.println("CHANGE STATUS: Task2 IN_PROGRESS->DONE");
@@ -55,10 +60,14 @@ public class Main {
    for (Task t : manager.getTasks ()) {
        System.out.println(t);
  }
+
     }
     public static void printAllTask(TaskManager manager){
         manager.getTasks();
         manager.getEpics();
         manager.getSubTasks();
     }
+
+
+
 }
--- src/TaskStatus.java
+++
@@ -1,5 +0,0 @@
-public enum TaskStatus {
-    NEW,
-    IN_PROGRESS,
-    DONE
-}
