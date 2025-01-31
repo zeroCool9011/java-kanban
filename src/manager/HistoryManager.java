b25441c4e40622bfa401ef445e6f28984224f70d
Zero Cool <andrewmackar0ff@yandex.ru>
2025-01-31 22:41:37 +0300

    Убран лимит истории, избавились от повторов в истории

--- src/manager/HistoryManager.java
+++ src/manager/HistoryManager.java
@@ -3,11 +3,12 @@ package manager;
 import model.Task;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public interface HistoryManager {
     void addHistory(Task task);
+
     void remove(int id);
+
     ArrayList<Task> getHistory();
 
 }
