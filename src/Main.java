0df897ddef0bb397115f07899644dd6b6f33690a
Zero Cool <andrewmackar0ff@yandex.ru>
2025-01-31 22:44:55 +0300

    Убран лимит истории, избавились от повторов в истории

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import manager.HistoryManager;
-import manager.InMemoryTaskManager;
 import manager.Managers;
 import manager.TaskManager;
 import model.TaskStatus;
@@ -11,21 +10,21 @@ import model.Task;
 public class Main {
     public static void main(String[] args) {
 
-    TaskManager manager = Managers.getDefault();
+        TaskManager manager = Managers.getDefault();
 
-    Task task1 = new Task("model.Task #1","Task1 description", TaskStatus.NEW);
-    Task task2 = new Task("model.Task #2","Task2 description", TaskStatus.IN_PROGRESS);
-    final int id1 = manager.addNewTask(task1).getId();
-    final int id2 = manager.addNewTask(task2).getId();
+        Task task1 = new Task("model.Task #1", "Task1 description", TaskStatus.NEW);
+        Task task2 = new Task("model.Task #2", "Task2 description", TaskStatus.IN_PROGRESS);
+        final int id1 = manager.addNewTask(task1).getId();
+        final int id2 = manager.addNewTask(task2).getId();
 
-    Epic epic1 =  new Epic("model.Epic#1","Epic1 description");
-    Epic epic2 =  new Epic("model.Epic#2","Epic2 description");
-    final int epicId1 = manager.addNewEpic(epic1).getId();
-    final int epicId2 = manager.addNewEpic(epic2).getId();
+        Epic epic1 = new Epic("model.Epic#1", "Epic1 description");
+        Epic epic2 = new Epic("model.Epic#2", "Epic2 description");
+        final int epicId1 = manager.addNewEpic(epic1).getId();
+        final int epicId2 = manager.addNewEpic(epic2).getId();
 
-    SubTask subTask1 = new SubTask("model.SubTask#1","SubTask1 description", TaskStatus.IN_PROGRESS,epicId1);
-    SubTask subTask2 = new SubTask("model.SubTask#2","SubTask2 description", TaskStatus.NEW, epicId1);
-    SubTask subTask3 = new SubTask("model.SubTask#3","SubTask3 description", TaskStatus.DONE, epicId2);
+        SubTask subTask1 = new SubTask("model.SubTask#1", "SubTask1 description", TaskStatus.IN_PROGRESS, epicId1);
+        SubTask subTask2 = new SubTask("model.SubTask#2", "SubTask2 description", TaskStatus.NEW, epicId1);
+        SubTask subTask3 = new SubTask("model.SubTask#3", "SubTask3 description", TaskStatus.DONE, epicId2);
         final int subTaskId1 = manager.addNewSubTask(subTask1).getId();
         final int subTaskId2 = manager.addNewSubTask(subTask2).getId();
         final int subTaskId3 = manager.addNewSubTask(subTask3).getId();
@@ -34,10 +33,7 @@ public class Main {
         final HistoryManager historyManager = Managers.getDefaultHistory();
 
 
-
         manager.deleteEpicId(3);
-
-
 
 
         // manager.deleteEpicId(4);
@@ -68,8 +64,6 @@ public class Main {
 //        System.out.println(manager.getEpics());
 
 
-
-
         final Task task7 = manager.getTask(2);
 //        final Task task8 = manager.getTask(2);
 //        final Task task9 = manager.getTask(2);
@@ -79,7 +73,7 @@ public class Main {
         //SubTask subTask21 = manager.getSubTask(7);
 
         manager.getSubTask(5);
-         manager.getEpic(4);
+        manager.getEpic(4);
         //SubTask subTask10 = manager.getSubTask(5);
         manager.getSubTasks();
 
@@ -89,15 +83,16 @@ public class Main {
         System.out.println("История вызова " + manager.getHistory() + "\n");
 
         task.setStatus(TaskStatus.DONE);
-    manager.updateTask(task);
-    System.out.println("CHANGE STATUS: Task2 IN_PROGRESS->DONE");
-        System.out.println( "Sanayw: ");
-   for (Task t : manager.getTasks ()) {
-       System.out.println(t);
-   }
+        manager.updateTask(task);
+        System.out.println("CHANGE STATUS: Task2 IN_PROGRESS->DONE");
+        System.out.println("Sanayw: ");
+        for (Task t : manager.getTasks()) {
+            System.out.println(t);
+        }
 
     }
-    public static void printAllTask(TaskManager manager){
+
+    public static void printAllTask(TaskManager manager) {
         manager.getTasks();
         manager.getEpics();
         manager.getSubTasks();
