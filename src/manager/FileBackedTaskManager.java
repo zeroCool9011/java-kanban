96280c608320ecec55d668620f787fbd26f1ba7d
Zero Cool <andrewmackar0ff@yandex.ru>
2025-02-16 12:34:16 +0300

    Исправил

--- src/manager/FileBackedTaskManager.java
+++ src/manager/FileBackedTaskManager.java
@@ -82,29 +82,29 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
 
     private static Task fromString(String value) {
         String[] linesTask = value.split(",", 6);
-            Integer id = Integer.parseInt(linesTask[0]);
-            String name = linesTask[2];
-            TasksType tasksType = TasksType.valueOf(linesTask[1]);
-            TaskStatus status = TaskStatus.valueOf(linesTask[3]);
-            String description = linesTask[4];
+        Integer id = Integer.parseInt(linesTask[0]);
+        String name = linesTask[2];
+        TasksType tasksType = TasksType.valueOf(linesTask[1]);
+        TaskStatus status = TaskStatus.valueOf(linesTask[3]);
+        String description = linesTask[4];
 
-            switch (tasksType) {
-                case TASK:
-                    return new Task(id, name, description, status);
+        switch (tasksType) {
+            case TASK:
+                return new Task(id, name, description, status);
 
-                case EPIC:
-                    return new Epic(id, name, description);
+            case EPIC:
+                return new Epic(id, name, description);
 
-                case SUBTASK:
-                    String epicIdString = linesTask[5].trim();
-                    if (epicIdString.isEmpty()) {
-                        throw new NumberFormatException("Epic ID отсутствует для SubTask");
-                    }
-                    return new SubTask(id, name, description, status, Integer.valueOf(epicIdString));
+            case SUBTASK:
+                String epicIdString = linesTask[5].trim();
+                if (epicIdString.isEmpty()) {
+                    throw new NumberFormatException("Epic ID отсутствует для SubTask");
+                }
+                return new SubTask(id, name, description, status, Integer.valueOf(epicIdString));
 
-                default:
-                    throw new RuntimeException("Ошибка: Неподдерживаемый тип задачи." );
-            }
+            default:
+                throw new RuntimeException("Ошибка: Неподдерживаемый тип задачи.");
+        }
 
     }
 
@@ -127,6 +127,7 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         Task newTask = super.addNewTask(task);
         save();
         return newTask;
+
     }
 
     @Override
